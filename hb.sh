c4c6ffd81d58aa80de37da7b144e461247f72987
Bruno Pinheiro <bspinheiro@users.noreply.github.com>
2022-05-24 01:02:59 -0300

    Create hb.sh

---
+++ hb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p ~/Applications ~/.homebrew && curl -L https://github.com/Homebrew/brew$
+eval "$(.homebrew/bin/brew shellenv)"
+brew update --force --quiet
+chmod -R go-w "$(brew --prefix)/share/zsh"
+echo "export PATH=$HOME/.homebrew/bin:$PATH" >> ~/.zshrc
+echo "export HOMEBREW_CASK_OPTS="--appdir=~/Applications" >> ~/.zshrc
+echo "export HOMEBREW_NO_ENV_HINTS=1" >> ~/.zshrc
+~/.homebrew/bin/brew install pidof
+~/.homebrew/bin/pidof terminal|xargs kill
